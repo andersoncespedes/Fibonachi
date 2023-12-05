eb3c3e96fc2c0451f4f0a2dbd961edafbbdf2b4f
anderson cespede <andersontrabajos2@gmail.com>
2023-12-04 21:16:13 -0500

    feat:

--- Program.cs
+++ Program.cs
@@ -9,6 +9,15 @@ public static class Program{
         }while(fib.Last() < num);
         return fib.Last() == num;
     }
+     public static void FibonacciList(int num){
+        List<int> fib = new List<int>(){0,1};
+        for(int i = 2; i <= num ; i++){
+            fib.Add(fib[fib.Count - 1] + fib[fib.Count - 2]);
+        }
+        foreach(var a in fib){
+            Console.WriteLine(a);
+        }
+    }
     public static int Fibonacci(int num){
         List<int> fib = new List<int>(){0,1};
         for(int i = 2; i <= num ; i++){
@@ -38,6 +47,13 @@ public static class Program{
             int num = int.Parse(Console.ReadLine());
             int res = Fibonacci(num);
             Console.WriteLine($"La secuencia numero {num} en fibonacci es {res}");
+            Console.WriteLine("Presione Enter Para Continuar ->");
+            Console.ReadLine();
+        }
+         else if(op == 3){
+            Console.WriteLine("Ingrese un Numero");
+            int num = int.Parse(Console.ReadLine());
+            FibonacciList(num);
             Console.WriteLine("Presione Enter Para Continuar ->");
             Console.ReadLine();
         }
